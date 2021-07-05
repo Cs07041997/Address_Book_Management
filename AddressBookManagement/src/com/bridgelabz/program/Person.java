d5c7d5acd6cf193093e7058778ac1b3bffddb262
Cs07041997 <84439789+Cs07041997@users.noreply.github.com>
2021-07-05 19:09:58 +0530

    AddressBookManagementProblem

---
+++ AddressBookManagement/src/com/bridgelabz/program/Person.java
@@ -0,0 +1,132 @@
+package com.bridgelabz.program;
+
+public class Person {
+	String FirstName;
+	String LastName;
+	String Address;
+	String City;
+    String State;
+    String Zip;
+    String PhoneNumber;
+	String Email;
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getZip() {
+		return Zip;
+	}
+	public void setZip(String zip) {
+		Zip = zip;
+	}
+	public String getPhoneNumber() {
+		return PhoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	@Override
+	public String toString() {
+		return "Person [FirstName=" + FirstName + ", LastName=" + LastName + ", Address=" + Address + ", City=" + City
+				+ ", State=" + State + ", Zip=" + Zip + ", PhoneNumber=" + PhoneNumber + ", Email=" + Email + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Address == null) ? 0 : Address.hashCode());
+		result = prime * result + ((City == null) ? 0 : City.hashCode());
+		result = prime * result + ((Email == null) ? 0 : Email.hashCode());
+		result = prime * result + ((FirstName == null) ? 0 : FirstName.hashCode());
+		result = prime * result + ((LastName == null) ? 0 : LastName.hashCode());
+		result = prime * result + ((PhoneNumber == null) ? 0 : PhoneNumber.hashCode());
+		result = prime * result + ((State == null) ? 0 : State.hashCode());
+		result = prime * result + ((Zip == null) ? 0 : Zip.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (Address == null) {
+			if (other.Address != null)
+				return false;
+		} else if (!Address.equals(other.Address))
+			return false;
+		if (City == null) {
+			if (other.City != null)
+				return false;
+		} else if (!City.equals(other.City))
+			return false;
+		if (Email == null) {
+			if (other.Email != null)
+				return false;
+		} else if (!Email.equals(other.Email))
+			return false;
+		if (FirstName == null) {
+			if (other.FirstName != null)
+				return false;
+		} else if (!FirstName.equals(other.FirstName))
+			return false;
+		if (LastName == null) {
+			if (other.LastName != null)
+				return false;
+		} else if (!LastName.equals(other.LastName))
+			return false;
+		if (PhoneNumber == null) {
+			if (other.PhoneNumber != null)
+				return false;
+		} else if (!PhoneNumber.equals(other.PhoneNumber))
+			return false;
+		if (State == null) {
+			if (other.State != null)
+				return false;
+		} else if (!State.equals(other.State))
+			return false;
+		if (Zip == null) {
+			if (other.Zip != null)
+				return false;
+		} else if (!Zip.equals(other.Zip))
+			return false;
+		return true;
+	}
+	
+	
+}
